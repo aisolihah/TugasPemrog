9aa2970a9fbaa0b41e11a4518a6ddaa21c0f6bb0
aisolihah <121341325+aisolihah@users.noreply.github.com>
2023-09-28 23:22:15 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,21 @@
+let identitas = "identitas"
+console.log("identitas")
+
+var nama = "ai solihah"
+let nim = "20220040032"
+
+console.log("nama     =  " + nama)
+console.log("nim      =  " + nim)
+
+// aritmatika
+
+let aritmatika = "aritmatika"
+console.log(aritmatika)
+
+let a = 20;
+let b = 5;
+
+console.log(a +  "  *  " + b + " = " +a*b)
+console.log(a +  "  +  " + b + " = " +(a+b))
+console.log(a +  "  -  " + b + " = " +(a-b))
+console.log(a +  "  /  " + b + " = " +(a/b))
