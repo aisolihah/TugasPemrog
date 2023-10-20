dcb6aca1ff4d8941b5e7054593e57d6d9ecef3fb
aisolihah <121341325+aisolihah@users.noreply.github.com>
2023-10-20 21:37:02 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,9 @@
+
+// N0 2 variabel nama restaurant yang bertipe array dengan ketentuan sama diatas .
+const restaurant = ["nama restaurant :Coffe break", "city : Sagaranten" , "favorite Drink : Ice Macca" , "favorite Food : Spaghetti noodles", "isVegan : true"]
+const[firstName, secondCity, thirdFavoriteDrink, fourthFavoritefood, fiveisVegan] = restaurant;
+console.log(firstName);
+console.log(secondCity);
+console.log(thirdFavoriteDrink);
+console.log(fourthFavoritefood);
+console.log(fiveisVegan);
---
+++ tugassesi3.js
@@ -0,0 +1,10 @@
+// NO 1 (membuat object dengan properties)
+
+const Restaurant = {
+    name: "Restaurant coffe break",
+    city: "sagaranten",
+    favoritedrink: "coffe latte",
+    favoritefood: "steak",
+    isVegan: "true",
+};
+console.table(Restaurant);
